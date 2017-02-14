5a530d27de5562d5e5698e5b93cdcb8c04ea12b4
Hongpeng Yu <480814177@qq.com>
2017-02-15 01:00:12 +0800

    下载完成后缓存到本地

--- DowneImage/DownOperation.m
+++ DowneImage/DownOperation.m
@@ -20,8 +20,17 @@
     @autoreleasepool {
         NSURL *url = [NSURL URLWithString:_urlStr];
         NSData *data = [NSData dataWithContentsOfURL:url];
-        
         sleep(5);
+        
+        // 下载完成缓存到本地
+#warning 缓存到本地, 只能缓存 property列表里的对象（包括 NSData, NSDate, NSNumber, NSString, NSArray, or NSDictionary）,图片需要转化为二进制
+        
+        /**
+         * path: 写入文件的路径
+         * useAuxiliaryFile: YES 为非原子写入,会有一个临时文件，NO
+         */
+        
+        [data writeToFile:[_urlStr urlPath] atomically:YES];
         
         UIImage *image = [UIImage imageWithData:data];
         
