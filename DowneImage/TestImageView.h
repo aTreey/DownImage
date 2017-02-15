ad5eeea73ff628e5c30fdf9a46e6e6912b514424
Hongpeng Yu <480814177@qq.com>
2017-02-16 00:02:41 +0800

    管理类中添加取消下载操作的方法;在自定义的operation中main方法的关键位置判断下载操作是否已经取消，如果取消直接return；增加自定义imageView设置图片

---
+++ DowneImage/TestImageView.h
@@ -0,0 +1,13 @@
+//
+//  TestImageView.h
+//  DowneImage
+//
+//  Created by Hongpeng Yu on 2017/2/15.
+//  Copyright © 2017年 Hongpeng Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestImageView : UIImageView
+
+@end
---
+++ DowneImage/TestImageView.m
@@ -0,0 +1,21 @@
+//
+//  TestImageView.m
+//  DowneImage
+//
+//  Created by Hongpeng Yu on 2017/2/15.
+//  Copyright © 2017年 Hongpeng Yu. All rights reserved.
+//
+
+#import "TestImageView.h"
+
+@implementation TestImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
